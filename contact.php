3ea9eecf0c32f335e3cbc98b1f98f9795104cf6f
ulysses156 <104098363+ulysses156@users.noreply.github.com>
2022-11-15 00:28:48 -0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,314 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="logo escuela renovado.png">
+	<title>Escuela Tecnica Nº7 Manuel Sadosky</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+		<div class="fh5co-loader"></div>
+	
+		<div id="page">
+		<nav class="fh5co-nav" role="navigation">
+			<div class="top">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 text-right">
+							<p class="num">NUMERO DE TELEFONO: 2664452000</p>
+							<p class="num">TELEFONO INTERNO: 6337</p>
+							<ul class="fh5co-social">
+								<li><a href="https://www.facebook.com/profile.php?id=100004170829407" target="_blank"><i class="icon-facebook"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-1">
+							<div id="fh5co-logo"><a href="index.html"><img src="logo escuela renovado.png" alt="logo"style="height: 100px;width: 100px;"></a></div>
+						</div>
+						<div class="col-xs-11 text-right menu-1">
+							<ul>
+								<li class="active"><a href="index.html">INICIO</a></li>
+								<li><a href="courses.html">NOTICIAS</a></li>
+								<li class="has-dropdown">
+									<a href="galeria videos.html">GALERIA VIDEOS</a>
+									<ul class="dropdown">
+										<li><a href="galeria videos_color.html">Galeria a color</a></li>
+										<li><a href="galeria videos.html">Galeria en blanco y negro</a></li>
+									
+									</ul>
+								</li>
+								<LI><a href="Projec.html">PROYECTOS</a></LI>
+							<li><a href="contact.html">UBICACION/CONTACTO</a></li>
+							<li class="has-dropdown">
+								<a href="Curiocidades.html">MAS</a>
+								<ul class="dropdown">
+									<li><a href="Curiocidades.html">Curiosidades</a></li>
+									<li><a href="biblioteca.html">Biblioteca</a></li>
+									<li><a href="Horarios.html">Horarios</a></li>
+								</ul>
+							</li>
+								<li class="btn-cta"><a href="https://classroom.google.com/u/0/h?hl=es" target="_blank"><span>CLASSROOM</span></a></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</nav>
+	
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Seccion De Contacto</h1>
+							<h2>Ingresaste A la Seccion De Contacto De La Escuela Tecnica Nº7 Manuel Sadosky, Desliza hacia Abajo Para Ver Mas.</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5 col-md-push-1 animate-box">
+					
+					<div class="fh5co-contact-info">
+						<h3>Informacion De Contacto</h3>
+						<ul>
+							<li class="address">Belgrano Nº 1633 <br>San Luis Argentina</li>
+							<li class="phone"><a href="tel://+542664452000">+542664452000</a></li>
+							<li class="email"><a href="">et7sanluis@gmail.com</a></li>
+							<li class="url"><a href="tel://6337">Int: 6337</a></li>
+						</ul>
+					</div>
+
+				</div>
+				<div class="col-md-6 animate-box">
+					<h3>Ponerse En Contacto</h3>
+					<form method="post" id="form">
+						<div class="row form-group">
+							<div class="col-md-6">
+								<!-- <label for="fname">First Name</label> -->
+								<input type="text" name="First_Name" id="fname" class="form-control" placeholder="Indica tu nombre">
+							</div>
+							<div class="col-md-6">
+								<!-- <label for="lname">Last Name</label> -->
+								<input type="text" name="Last_Name" id="lname" class="form-control" placeholder="Indica tu apellido">
+							</div>
+						</div>
+
+						<div class="row form-group">
+							<div class="col-md-12">
+								<!-- <label for="email">Email</label> -->
+								<input type="text" name="email" id="email" class="form-control" placeholder="Correo electronico">
+							</div>
+						</div>
+
+						<div class="row form-group">
+							<div class="col-md-12">
+								<!-- <label for="subject">Subject</label> -->
+								<input type="text"  name="Subject" id="subject" class="form-control" placeholder="Indica cual es tu asunto">
+							</div>
+						</div>
+
+						<div class="row form-group">
+							<div class="col-md-12">
+								<!-- <label for="message">Message</label> -->
+								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Explicanos tu problema o duda..."></textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<input type="submit"  name="register" value="Enviar Mensaje" class="btn btn-primary">
+						</div>
+
+					</form>	
+				</div>
+		<?php
+		include("php/registrar.php");
+		?>	
+			</div>
+			
+		</div>
+	</div>
+	<!-- 
+	<div id="map" class="fh5co-map"></div>
+	<div id="fh5co-started" style="background-image:url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Lets Get Started</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<p><a href="#" class="btn btn-default btn-lg">Create A Free Course</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+-->
+<!-- mi mapa --> 
+<center><div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3334.477459029923!2d-66.3490565855612!3d-33.30633099751463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95d4399123bb9d07%3A0x9cf05827d0773778!2sEscuela%20T%C3%A9cnica%20N%C2%B0%207%20%22Dr.%20Manuel%20Sadosky%22!5e0!3m2!1ses!2sar!4v1667763017861!5m2!1ses!2sar" width="1200" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/">divi discount</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:1200px;}</style><a href="https://www.embedgooglemap.net">google maps for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:1200px;}</style></div></div></center>
+
+<footer id="fh5co-footer" role="contentinfo">
+	<div class="container">
+		<div class="row row-pb-md">
+			<div class="col-md-3 fh5co-widget">
+				<h4>Sobre El Aprendizaje En Nuestro Colegio</h4>
+				<p>Harás una recorrida por distintos talleres</p>
+				<p>
+					En estos talleres aprenderas informatica,electricidad,mecanica,electronica, y mucho mas..
+				</p>
+			</div>
+			<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+				<h4>INICIO</h4>
+				<ul class="fh5co-footer-links">
+					<li><a href="biblioteca/index.html">Biblioteca</a></li>
+					<li><a href="Projec.html">Proyectos</a></li>
+					<li><a href="contact.html">Contacto</a></li>
+					
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+				<h4>INFORMACION</h4>
+				<ul class="fh5co-footer-links">
+					<li><a href="Projec.html">Proyectos</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+				<h4>REDES SOCIALES</h4>
+				<ul class="fh5co-footer-links">
+					<li><a href="https://www.facebook.com/profile.php?id=100004170829407" target="_blank">Facebook</a></li>
+					<li><a href="#">Numero De Telefono</a></li>
+					<li><a href="#">Numero Interno</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+				<h4>PROYECTOS</h4>
+				<ul class="fh5co-footer-links">
+					<li><a href="Projec.html">Proyectos 2022</a></li>
+					<li><a href="#">Proyectos 2023 (Futura Promocion)</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row copyright">
+			<div class="col-md-12 text-center">
+				<p>
+					<small class="block">&copy; Escuela Tecnica Nº7 Manuel Sadosky</small> 
+					<small class="block"> <a href="http://freehtml5.co/" target="_blank"><a href="http://unsplash.co/" target="_blank"></a></small>
+				</p>
+				<p>
+					<ul class="fh5co-social-icons">
+						<li><a href="https://www.facebook.com/profile.php?id=100004170829407" target="_blank"><i class="icon-facebook"></i></a></li>
+					</ul>
+				</p>
+			</div>
+		</div>
+
+	</div>
+</footer>
+</div>
+
+<div class="gototop js-top">
+	<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+</div>
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
